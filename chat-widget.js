736d68d10ba6072925c515eb99d13836397ff05f
adiyadorj badamsereejid <91112558+Zombie-01@users.noreply.github.com>
2025-07-01 00:50:04 -0700

    Update chat-widget.js

--- chat-widget.js
+++ chat-widget.js
@@ -18,7 +18,7 @@
 
   // Create iframe
   const iframe = document.createElement("iframe");
-  iframe.src = "https://test-widget-one.vercel.app/chat?lang=auto"; // Change to your chat app URL
+  iframe.src = "https://app.voyager.mn?lang=auto"; // Change to your chat app URL
   iframe.style = `
     position: fixed;
     bottom: 70px;
